665cae90b5799ad84863de01a37ae977002ac847
Saleh Kalouti <salehkalouti@gmail.com>
2023-02-16 11:34:03 +0200

    tasks

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,16 @@
+
+function Box(props) {
+    const boxStyle = {
+        backgroundColor: props.color,
+        height: props.height,
+        width: props.width,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center"
+    };
+    return <div style={boxStyle}>{props.children}</div>
+}
+
+
+export default Box
